ddd0fa0901c50238f5d1da15e521525ece5d9947
Gray Mafutala <graymafutala@gmail.com>
2023-02-15 19:51:04 +0300

    Small semantic correction and selection of tags to animate page on loading.

---
+++ src/js/display_animation.js
@@ -0,0 +1,11 @@
+// To animate the page at loading
+
+const header = document.querySelector('.world-header');
+
+const leftEllipse = document.querySelector('.main-content__box--left');
+const centerEllipse = document.querySelector('.main-content__box--center');
+const rightEllipse = document.querySelector('.main-content__box--right');
+
+const footerTitle = document.querySelector('.footer__title');
+const footerPara = document.querySelector('.footer__para');
+const footerCirclesImg = document.querySelector('.footer__right-content');
