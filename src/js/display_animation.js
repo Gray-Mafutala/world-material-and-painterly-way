f85ee6cd2dc1d72a37fe28ad07754cce58bd79ab
Gray Mafutala <graymafutala@gmail.com>
2023-02-15 21:17:40 +0300

    Creation of animations on loading

--- src/js/display_animation.js
+++ src/js/display_animation.js
@@ -1,6 +1,6 @@
 // To animate the page at loading
 
-const header = document.querySelector('.world-header');
+const header = document.querySelector('.world-header__navbar');
 
 const leftEllipse = document.querySelector('.main-content__box--left');
 const centerEllipse = document.querySelector('.main-content__box--center');
@@ -9,3 +9,17 @@ const rightEllipse = document.querySelector('.main-content__box--right');
 const footerTitle = document.querySelector('.footer__title');
 const footerPara = document.querySelector('.footer__para');
 const footerCirclesImg = document.querySelector('.footer__right-content');
+
+const startAnim = () => {
+    header.classList.add('header_animation');
+
+    leftEllipse.classList.add('ellipse-anim');
+    centerEllipse.classList.add('ellipse-anim');
+    rightEllipse.classList.add('ellipse-anim');
+
+    footerTitle.classList.add('footer-element-anim');
+    footerPara.classList.add('footer-element-anim');
+    footerCirclesImg.classList.add('footer-element-anim');
+};
+
+startAnim();
